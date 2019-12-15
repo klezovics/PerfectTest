da8e170f2c9676000c29ff42e696ba8f688fb48a
Arthur K <klezovich@gmail.com>
2019-12-15 21:29:51 +0100

    Initial commit

---
+++ src/main/java/com/klezovich/perfecttest/PerfectTestApplication.java
@@ -0,0 +1,13 @@
+package com.klezovich.perfecttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfectTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/klezovich/perfecttest/PerfectTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klezovich.perfecttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfectTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
