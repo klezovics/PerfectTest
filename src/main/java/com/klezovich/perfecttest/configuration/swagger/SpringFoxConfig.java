9cef9ddfedf059f44c520d6332851dc20d632822
aklezovics <arturs.klezovics@raisin.com>
2019-12-16 18:23:16 +0100

    Declutter the whole thing

--- src/main/java/com/klezovich/perfecttest/configuration/swagger/SpringFoxConfig.java
+++ src/main/java/com/klezovich/perfecttest/configuration/swagger/SpringFoxConfig.java
@@ -2,8 +2,6 @@ package com.klezovich.perfecttest.configuration.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -17,23 +15,9 @@ public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    @Bean
-    public WebMvcConfigurerAdapter adapter() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addResourceHandlers(ResourceHandlerRegistry registry) {
-                registry.addResourceHandler("swagger-ui.html")
-                        .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
-                registry.addResourceHandler("/webjars/**")
-                        .addResourceLocations("classpath:/META-INF/resources/webjars/");
-                super.addResourceHandlers(registry);
-            }
-        };
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.klezovich.perfecttest"))
+            .paths(PathSelectors.any())
+            .build();
     }
 }
