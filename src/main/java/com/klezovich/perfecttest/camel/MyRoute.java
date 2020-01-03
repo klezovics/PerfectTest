a8922f3de9d67453d2b6d307dcfbeac5a5c1ddda
aklezovics <arturs.klezovics@raisin.com>
2020-01-03 15:03:14 +0100

    Add Camel support and first Camel route

---
+++ src/main/java/com/klezovich/perfecttest/camel/MyRoute.java
@@ -0,0 +1,14 @@
+package com.klezovich.perfecttest.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("file:/Users/aklezovics/in?noop=true")
+            .to("file:/Users/aklezovics/out");
+    }
+}
